a863a60cd74926b2b08d36afe837cf7e039fa538
Jake Stebbings <jake@jdesign.co>
2013-02-26 21:39:01 +0000

    Inital Release

---
+++ inc/contactform.config.php
@@ -0,0 +1,14 @@
+<?php
+$cfg['email'] = '';
+$cfg['mailfrom'] = '';
+$cfg['emailaddress_cc'] = '';
+$cfg['emailaddress_bcc'] = '';
+$cfg['form_validationmessage'] = '';
+$cfg['form_error_captcha'] = 'Value does not match';
+$cfg['form_error_emptyfield'] = 'This field can\'t be left empty';
+$cfg['form_error_invalidemailaddress'] = 'Invalid email address';
+$cfg['form_emailnotificationtitle'] = '';
+$cfg['form_emailnotificationmessage'] = '';
+$cfg['form_emailnotificationinputid'] = '';
+$cfg['dir_contactform'] = 'contactform';
+?>
